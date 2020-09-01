fb4e125519ce94a88d2065d920de6ba395265a74
Rhea County High School <63121534+RCHS-Comp@users.noreply.github.com>
2020-08-31 23:02:40 -0400

    Update TWLZONE.sh

--- TWLZONE.sh
+++ TWLZONE.sh
@@ -7,7 +7,7 @@ pkg update; pkg upgrade                                # Update the already inst
                                                        #
 pkg install xterm xorg-xcalc xorg-xclock i3 tigervnc   # Add the packages
                                                        #
-echo "vncserver -localhost :1" > ~/start_server           # Make start server script
+echo "vncserver -localhost :1" > ~/start_server        # Make start server script
 echo "export DISPLAY=\":1\"" >> ~/start_server         #
 echo "vncserver -kill :1" > ~/stop_server              # Make stop server script
 chmod 777 ~/start_server ~/stop_server                 #
@@ -15,6 +15,7 @@ chmod 777 ~/start_server ~/stop_server                 #
 echo "exec i3" > ~/.xinitrc                            # Make xinitrc
 mkdir -p ~/.vnc                                        # Create VNC config folder if not exist
 echo "i3 &" > ~/.vnc/xstartup                          # Overwrite VNC startup
+chmod 777 ~/.vnc/xstartup                              #
 ~/start_server                                         # Start VNC server / Config for first time
 echo "Connect to localhost:5901 via VNC"               # Tell user where to connect to
 echo "Replace 1 with your current display."            #
